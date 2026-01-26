769961677e6020cc64a4b1ff67310dd0ad04a04e
Damian Aebi <damian.aebi@nexplore.ch>
2026-01-26 15:30:05 +0100

    user model

--- src/main/java/com/damiansprojekt/dreamshops/model/User.java
+++ src/main/java/com/damiansprojekt/dreamshops/model/User.java
@@ -1,4 +1,25 @@
 package com.damiansprojekt.dreamshops.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders;
 }
